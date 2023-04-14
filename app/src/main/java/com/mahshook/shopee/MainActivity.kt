6154b8c5d62a850485296e1e50ee2a664e689896
123mahshook <mahshookrk@gmail.com>
2023-04-14 14:45:58 +0530

    first commit

---
+++ app/src/main/java/com/mahshook/shopee/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.mahshook.shopee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.mahshook.shopee.ui.theme.ShopeeTheme
+import com.mahshook.shopee.ui.theme.initSize
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initSize()
+        setContent {
+            ShopeeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    Surface(modifier = Modifier
+        .background(Color.White)
+        .fillMaxSize()
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Greeting()
+        }
+
+    }
+}
+
+@Composable
+fun Greeting() {
+    val greetingState= remember {
+        mutableStateListOf<String>("mahshook","jack")
+    }
+    for(item in greetingState){
+        Text(item)
+    }
+    Button(onClick = { greetingState.add("Tony") }) {
+        Text("Addd name")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ShopeeTheme {
+        MainScreen()
+    }
+}
